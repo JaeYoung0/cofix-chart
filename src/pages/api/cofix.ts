d50690479c09e663da1ad7272d89714b9c9635fc
JaeYoung0 <hjy8697@gmail.com>
2022-12-02 04:37:54 +0900

    Fix: cofix page getStaticProps

--- src/pages/api/cofix.ts
+++ src/pages/api/cofix.ts
@@ -23,38 +23,53 @@ export default async (
     const year = req.query.year as string
     const cofixType = req.query.cofixType as CofixType
 
-    try {
-      const response = await fetch(
-        `https://portal.kfb.or.kr/fingoods/cofix.php?BasicYear=${year}&BasicYear_W=${year}`
-      )
-      const htmlString = await response.text()
-      const $ = cheerio.load(htmlString)
-      const data: CofixRow[] = []
+    const crawledData = await crawlCofixData({ cofixType, year: Number(year) })
 
-      if (cofixType === 'short') {
-        $(getTargetContext(cofixType)).each((idx, el) => {
-          if (idx === 0) return // ignore title row
-          const tds = $(el).find('td') // 공시일 | 대상기간 | 단기 COFIX
-          const period = $(tds[1]).text() // "2022/11/12 ~ 2022/11/18"
-          const [startDate, endDate] = period.split(' ~ ')
-          const value = $(tds[2]).text()
-          const tableRow = { startDate, endDate, value }
-          data.push(tableRow)
-        })
-      } else if (cofixType === 'new') {
-        $(getTargetContext(cofixType)).each((idx, el) => {
-          if (idx === 0) return // ignore title row
-          const tds = $(el).find('td') // 공시일 | 대상월 | 신규취급액기준 COFIX | 잔액기준 COFIX | 신 잔액기준 COFIX
-          const startDate = $(tds[1]).text()
-          const endDate = ''
-          const value = $(tds[4]).text()
-          const tableRow = { startDate, endDate, value }
-          data.push(tableRow)
-        })
-      }
-      res.status(200).json({ result: data.reverse() })
-    } catch (error) {
-      res.status(404).end()
+    res.status(200).json(crawledData ?? { result: [{ startDate: '', endDate: '', value: '' }] })
+  }
+}
+
+export const crawlCofixData = async ({
+  cofixType,
+  year,
+}: {
+  cofixType: CofixType
+  year: number
+}) => {
+  try {
+    const response = await fetch(
+      `https://portal.kfb.or.kr/fingoods/cofix.php?BasicYear=${year}&BasicYear_W=${year}`
+    )
+    const htmlString = await response.text()
+    const $ = cheerio.load(htmlString)
+    const data: CofixRow[] = []
+
+    if (cofixType === 'short') {
+      $(getTargetContext(cofixType)).each((idx, el) => {
+        if (idx === 0) return // ignore title row
+        const tds = $(el).find('td') // 공시일 | 대상기간 | 단기 COFIX
+        const period = $(tds[1]).text() // "2022/11/12 ~ 2022/11/18"
+        const [startDate, endDate] = period.split(' ~ ')
+        const value = $(tds[2]).text()
+        const tableRow = { startDate, endDate, value }
+        data.push(tableRow)
+      })
+    } else if (cofixType === 'new') {
+      $(getTargetContext(cofixType)).each((idx, el) => {
+        if (idx === 0) return // ignore title row
+        const tds = $(el).find('td') // 공시일 | 대상월 | 신규취급액기준 COFIX | 잔액기준 COFIX | 신 잔액기준 COFIX
+        const startDate = $(tds[1]).text()
+        const endDate = ''
+        const value = $(tds[4]).text()
+        const tableRow = { startDate, endDate, value }
+        data.push(tableRow)
+      })
     }
+
+    return { result: data.reverse() ?? [] }
+  } catch (err) {
+    console.log('@@err', err)
+
+    console.error(err)
   }
 }
--- src/pages/cofix/index.tsx
+++ src/pages/cofix/index.tsx
@@ -1,6 +1,6 @@
 import Cofix from '@/ui/pages/Cofix'
-import { fetchCofix } from '@/services/cofix'
 import { dehydrate, QueryClient } from '@tanstack/react-query'
+import { crawlCofixData } from '../api/cofix'
 
 function CofixPage() {
   return <Cofix />
@@ -11,7 +11,15 @@ export default CofixPage
 export async function getStaticProps() {
   const queryClient = new QueryClient()
 
-  await queryClient.prefetchQuery(['short', 2022], () => fetchCofix(`?cofixType=short&year=2022`))
+  await Promise.all([
+    queryClient.prefetchQuery(['short', 2022], () =>
+      crawlCofixData({ cofixType: 'short', year: 2022 }).then((res) => res?.result)
+    ),
+
+    queryClient.prefetchQuery(['new', 2022], () =>
+      crawlCofixData({ cofixType: 'new', year: 2022 }).then((res) => res?.result)
+    ),
+  ])
 
   return {
     props: {
